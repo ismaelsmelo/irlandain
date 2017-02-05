fe840d0f24edd4561d552a9f735751431e42e6fc
ismaelsmelo <ismael.melo@onsizing.com>
2017-02-05 19:02:04 -0200

    teste

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-<!DOCTYPE html>
-<html>
+var express = require('express');
+var app = express();
 
-  <head>
-    <style type="text/css">
-      html, body { height: 100%; margin: 0; padding: 0; }
-      #map { height: 100%; }
-    </style>
-  </head>
+app.set('port', (process.env.PORT || 5000));
 
-  <body>
-    <div id="map"></div>
-    <script type="text/javascript">
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 53.335474, lng: -6.280748},
-          zoom: 8
-        });
-      }
-    </script>
-    <script async defer
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC2W3q_XvHB6M-hamGZ9S9FiasfBrOyxSM&callback=initMap">
-    </script>
-  </body>
-</html>
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
